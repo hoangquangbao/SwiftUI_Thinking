6c3c27c730b3f973bc6c942778711044172328b2
Bao Hoang <hoangquangbao.developer@gmail.com>
2022-11-27 15:53:37 +0700

    Initialed Project

---
+++ SwiftUI_Thinking/SwiftUI_ThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ThinkingApp.swift
+//  SwiftUI_Thinking
+//
+//  Created by Quang Bao on 27/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
