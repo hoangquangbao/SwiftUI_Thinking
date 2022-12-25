f703cc7f7af4e086a2650fbc0385610b62858137
Bao Hoang <hoangquangbao.developer@gmail.com>
2022-12-25 17:26:43 +0700

    Animation count for indicator

--- SwiftUI_Thinking/TimerBootcamp.swift
+++ SwiftUI_Thinking/TimerBootcamp.swift
@@ -9,32 +9,35 @@ import SwiftUI
 
 struct TimerBootcamp: View {
     
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let timer = Timer.publish(every: 0.3, on: .main, in: .common).autoconnect()
     
     //MARK: - Current time
 //    @State var currentDate = Date()
-    var dateFormatter: DateFormatter {
-        let formatter = DateFormatter()
+//    var dateFormatter: DateFormatter {
+//        let formatter = DateFormatter()
 //        formatter.dateStyle = .medium
-        formatter.timeStyle = .medium
-        return formatter
-    }
+//        formatter.timeStyle = .medium
+//        return formatter
+//    }
     
     //MARK: - Countdown
 //    @State var count: Int = 10
 //    @State var finishedText: String? = nil
     
     //MARK: - Countdown to future date
-    @State var timeRemaining: String = ""
-    @State var futureDate: Date = (Calendar.current.date(byAdding: .hour, value: 1, to: Date()) ?? Date())
+//    @State var timeRemaining: String = ""
+//    @State var futureDate: Date = (Calendar.current.date(byAdding: .hour, value: 1, to: Date()) ?? Date())
     
-    func updateTimeRemaining() {
-        let remaining = Calendar.current.dateComponents([.hour, .minute, .second], from: Date(), to: futureDate)
-        let hour = remaining.hour ?? 0
-        let minute = remaining.minute ?? 0
-        let second = remaining.second ?? 0
-        timeRemaining = "\(hour):\(minute):\(second)"
-    }
+    //MARK: - Animation count
+    @State var count: Int = 0
+    
+//    func updateTimeRemaining() {
+//        let remaining = Calendar.current.dateComponents([.hour, .minute, .second], from: Date(), to: futureDate)
+//        let hour = remaining.hour ?? 0
+//        let minute = remaining.minute ?? 0
+//        let second = remaining.second ?? 0
+//        timeRemaining = "\(hour):\(minute):\(second)"
+//    }
     
     var body: some View {
         ZStack {
@@ -45,14 +48,26 @@ struct TimerBootcamp: View {
                 endRadius: 350)
             .edgesIgnoringSafeArea(.all)
             
-            Text(timeRemaining)
-                .font(.system(size: 50, weight: .bold, design: .rounded))
-                .foregroundColor(.white)
-                .lineLimit(1)
-                .minimumScaleFactor(0.1)
+            HStack(spacing: 15) {
+                Circle()
+                    .offset(y: count == 1 ? -20 : 0)
+                Circle()
+                    .offset(y: count == 2 ? -20 : 0)
+                Circle()
+                    .offset(y: count == 3 ? -20 : 0)
+            }
+            .foregroundColor(.white)
+            .frame(width: 150)
         }
         .onReceive(timer) { _ in
-            updateTimeRemaining()
+            withAnimation {
+//                if count == 3 {
+//                    count = 0
+//                } else {
+//                    count += 1
+//                }
+                count = count == 3 ? 0 : count + 1
+            }
         }
     }
 }
