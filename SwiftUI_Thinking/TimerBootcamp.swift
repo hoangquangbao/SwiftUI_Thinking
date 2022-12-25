8a289a6c74b78160de61e82ef019f77c18f320a0
Bao Hoang <hoangquangbao.developer@gmail.com>
2022-12-25 17:01:11 +0700

    Countdown to date

--- SwiftUI_Thinking/TimerBootcamp.swift
+++ SwiftUI_Thinking/TimerBootcamp.swift
@@ -13,16 +13,28 @@ struct TimerBootcamp: View {
     
     //MARK: - Current time
 //    @State var currentDate = Date()
-//    var dateFormatter: DateFormatter {
-//        let formatter = DateFormatter()
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
 //        formatter.dateStyle = .medium
-//        formatter.timeStyle = .medium
-//        return formatter
-//    }
+        formatter.timeStyle = .medium
+        return formatter
+    }
     
     //MARK: - Countdown
-    @State var count: Int = 10
-    @State var finishedText: String? = nil
+//    @State var count: Int = 10
+//    @State var finishedText: String? = nil
+    
+    //MARK: - Countdown to future date
+    @State var timeRemaining: String = ""
+    @State var futureDate: Date = (Calendar.current.date(byAdding: .hour, value: 1, to: Date()) ?? Date())
+    
+    func updateTimeRemaining() {
+        let remaining = Calendar.current.dateComponents([.hour, .minute, .second], from: Date(), to: futureDate)
+        let hour = remaining.hour ?? 0
+        let minute = remaining.minute ?? 0
+        let second = remaining.second ?? 0
+        timeRemaining = "\(hour):\(minute):\(second)"
+    }
     
     var body: some View {
         ZStack {
@@ -33,18 +45,14 @@ struct TimerBootcamp: View {
                 endRadius: 350)
             .edgesIgnoringSafeArea(.all)
             
-            Text(finishedText ?? "\(count)")
+            Text(timeRemaining)
                 .font(.system(size: 50, weight: .bold, design: .rounded))
                 .foregroundColor(.white)
                 .lineLimit(1)
                 .minimumScaleFactor(0.1)
         }
         .onReceive(timer) { _ in
-            if count < 1 {
-                finishedText = "Happy New Year 2023 !!!"
-            } else {
-                count -= 1
-            }
+            updateTimeRemaining()
         }
     }
 }
