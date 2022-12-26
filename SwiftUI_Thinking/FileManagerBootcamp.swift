2f90f44e4b4dbde09af3c881a982d7208b7d3a6d
Bao Hoang <hoangquangbao.developer@gmail.com>
2022-12-26 16:20:18 +0700

    Inited file

---
+++ SwiftUI_Thinking/FileManagerBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  FileManagerBootcamp.swift
+//  SwiftUI_Thinking
+//
+//  Created by Quang Bao on 26/12/2022.
+//
+
+import SwiftUI
+
+struct FileManagerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FileManagerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FileManagerBootcamp()
+    }
+}
