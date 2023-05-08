0bfbb99f0b6aaa75d667c3b829ce9426ac043a44
Quang Bao <hoangquangbao.developer@gmail.com>
2023-05-08 17:08:21 +0700

    QuadCurve to builf graph

--- SwiftUI_Thinking/CustomCurvesBootcamp.swift
+++ SwiftUI_Thinking/CustomCurvesBootcamp.swift
@@ -33,10 +33,28 @@ struct Curves: Shape {
     }
 }
 
+struct QuadSample: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.midY))
+            path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.midY),
+                              control: CGPoint(x: rect.width * 0.25, y: rect.height * 0.25))
+            path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.midY),
+                              control: CGPoint(x: rect.width * 0.75, y: rect.height * 0.75))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
+}
+
 struct CustomCurvesBootcamp: View {
     var body: some View {
-        Curves()
-            .frame(width: 200, height: 200)
+        QuadSample()
+            .fill(LinearGradient(colors: [.blue, .white], startPoint: .leading, endPoint: .trailing))
+            .ignoresSafeArea()
+        
+//        Curves()
+//            .frame(width: 100, height: 100)
     }
 }
 
