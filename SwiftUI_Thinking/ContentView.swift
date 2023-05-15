6150fb91681bea61615645505542f5961b7b5071
Quang Bao <hoangquangbao.developer@gmail.com>
2023-05-15 08:41:58 +0700

    Asymmetric is good transitions

--- SwiftUI_Thinking/ContentView.swift
+++ SwiftUI_Thinking/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var onOff: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            if onOff {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 250, height: 250)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                    .transition(.move(edge: .leading))
+//                    .transition(.scale)
+                    .transition(.asymmetric(insertion: .move(edge: .top), removal: .scale))
+            }
+            
+            Button {
+                withAnimation(.spring()) {
+                    onOff.toggle()
+                }
+            } label: {
+                Text("Click here")
+            }
+
         }
         .padding()
     }
