e46025fa4e2e661b2c3675484a2b46e5fcfec473
Bao Hoang <hoangquangbao.developer@gmail.com>
2023-05-30 13:59:26 +0700

    Mathematic Operation

--- SwiftUI_Thinking/CombineBootcampView.swift
+++ SwiftUI_Thinking/CombineBootcampView.swift
@@ -56,67 +56,89 @@ class CombineBootcampViewModel: ObservableObject {
 //        dataService.currentValuePublisher
         dataService.passThrounghPublisher
         
-    //MARK: - Sequence Operation
+        //MARK: - Sequence Operation
         /*
         // FIRST
-//            .first()
-//            .first(where: {  $0 > 8 })
+            .first()
+            .first(where: {  $0 > 8 })
         /// If conditionals come before then publisher emit that value
-//            .tryFirst(where: { int in
-//                if int == 5 {
-//                    throw URLError(.badServerResponse)
-//                }
-//                return int > 7
-//            })
+            .tryFirst(where: { int in
+                if int == 5 {
+                    throw URLError(.badServerResponse)
+                }
+                return int > 7
+            })
         
         // LAST
-//            .last()
-//            .last(where: { int in
-//                int > 3
-//            })
+            .last()
+            .last(where: { int in
+                int > 3
+            })
         /// It will through to the end of the array before return a value
-//            .tryLast(where: { int in
-//                if int == 6 {
-//                    throw URLError(.badServerResponse)
-//                }
-//                return int > 4
-//            })
+            .tryLast(where: { int in
+                if int == 6 {
+                    throw URLError(.badServerResponse)
+                }
+                return int > 4
+            })
         
         // DROP: Loại những value thoả điều kiện
-//            .dropFirst()
+            .dropFirst()
         /// 1
-//            .dropFirst(5)
+            .dropFirst(5)
         /// 6,7,8,9,10
-//            .drop(while: { $0 < 5 })
+            .drop(while: { $0 < 5 })
         /// 5,6,7,8,9,10
-//            .tryDrop(while: { int in
-//                if int == 5 {
-//                    throw URLError(.badServerResponse)
-//                }
-//                return int < 8
-//            })
+            .tryDrop(while: { int in
+                if int == 5 {
+                    throw URLError(.badServerResponse)
+                }
+                return int < 8
+            })
         /// printf URLError
         
         // PREFIT: Lấy những giá trị thoả điều kiện
-//            .prefix(6)
+            .prefix(6)
         /// 1,2,3,4,5,6
-//            .prefix(while: { $0 < 5 })
+            .prefix(while: { $0 < 5 })
         /// 1,2,3,4
-//            .tryPrefix(while: { int in
-//                if int == 5 {
-//                    throw URLError(.badServerResponse)
-//                }
-//                return int < 8
-//            })
+            .tryPrefix(while: { int in
+                if int == 5 {
+                    throw URLError(.badServerResponse)
+                }
+                return int < 8
+            })
         /// 1,2,3,4 and URLError
         
         // OUTPUT
-//            .output(at: 4)
+            .output(at: 4)
         /// 5
-//            .output(in: 2...6)
+            .output(in: 2...6)
         /// 3,4,5,6,7
         */
         
+        //MARK: - Mathematic Operation
+        /*
+        // MAX
+//            .max()
+//            .max(by: {  $0 < $1 })
+//            .tryMax(by: { int1, int2 in
+//                if int1 == 5 {
+//                    throw URLError(.badServerResponse)
+//                }
+//                return int1 < int2
+//            })
+        
+        // MIN
+//            .min()
+//            .min(by: { $0 < $1 })
+//            .tryMin(by: { int1, int2 in
+//                if int1 == 5 {
+//                    throw URLError(.badServerResponse)
+//                }
+//                return int1 < int2
+//            })
+        */
             .map({ String($0) })
             .sink { completion in
                 switch completion {
