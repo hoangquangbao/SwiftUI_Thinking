9deb8ed356837c1c64b76c18f4d09ef6d97d1ab9
Bao Hoang <hoangquangbao.developer@gmail.com>
2023-05-31 14:58:07 +0700

    Get data with @escapingClosure and Combine. Convert @@escapingClosure to Combine

---
+++ SwiftUI_Thinking/FuturesBootcampView.swift
@@ -0,0 +1,95 @@
+//
+//  FuturesBootcampView.swift
+//  SwiftUI_Thinking
+//
+//  Created by Quang Bao on 31/05/2023.
+//
+
+import SwiftUI
+import Combine
+
+//Download with combine
+//Download with @escaping Closure
+//Conver @escaping Closure to Combine
+//If your project does not support Publisher and Subcriber Combine the use @escaping Closure but hightly recommend use Combine framework if you can because it clear and easy to control data download from internet and API.
+
+class FuturesBootcampViewModel: ObservableObject {
+    
+    @Published var title: String = "Starting title"
+    let url = URL(string: "https://www.google.com")!
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        download()
+    }
+    
+    func download() {
+        
+        /// Get data with Combine
+//        getCombinePublisher()
+        /// Get data with Future and Promise
+        getFuturePublisher()
+        
+        /// General part
+            .sink { _ in
+
+            } receiveValue: { [weak self] returnValue in
+                self?.title = returnValue
+            }
+            .store(in: &cancellables)
+        
+        /// Get data with @escaping Closure
+//        getEscapingClosure { [weak self] returnValue, error in
+//            if let error = error {
+//                print("ERROR: \(error)")
+//            } else {
+//                self?.title =  returnValue
+//            }
+//        }
+
+    }
+    
+    func getCombinePublisher() -> AnyPublisher<String, URLError> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .timeout(1, scheduler: DispatchQueue.main)
+            .map { _ in
+                return "New Value of Combine"
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func getEscapingClosure(completionHandle: @escaping (_ returnValue: String, _ error: Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { Data, response, error in
+            completionHandle("New Value of Closure", nil)
+        }
+        .resume()
+    }
+    
+    ///Convert @escaping Closure to Combine using Future/Promise
+    func getFuturePublisher() -> Future<String, Error> {
+        Future { promise in
+            self.getEscapingClosure { returnValue, error in
+                if let error = error {
+                    promise(.failure(error))
+                } else {
+                    promise(.success(returnValue))
+                }
+            }
+        }
+    }
+}
+
+struct FuturesBootcampView: View {
+    
+    @StateObject var vm = FuturesBootcampViewModel()
+    
+    var body: some View {
+        Text(vm.title)
+    }
+}
+
+struct FuturesBootcampView_Previews: PreviewProvider {
+    static var previews: some View {
+        FuturesBootcampView()
+    }
+}
